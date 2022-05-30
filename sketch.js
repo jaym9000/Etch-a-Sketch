4b578ee4754f5fa7bccbc5ff69e9d82eaf2f39b4
Jay-M <jean_marcc@hotmail.com>
2022-05-30 15:44:07 -0300

    Working on creating 16x16 grid of square divs

---
+++ sketch.js
@@ -0,0 +1,7 @@
+const container = document.querySelector(".grid");
+
+for(let i = 0; i<16; i++){
+    const cell = document.createElement("div");
+    cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    container.appendChild(cell);
+}
