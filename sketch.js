8c4b79b9b045b6f5a8dbdc2c3584bc130a0e0d61
Jay-M <jean_marcc@hotmail.com>
2022-06-05 00:04:49 -0300

    made a grid with divs using js and some titles.

--- sketch.js
+++ sketch.js
@@ -1,9 +1,12 @@
-const container = document.querySelector(".grid");
+const container = document.querySelector(".container");
 
+// for loop inside for loop creates a 16 x 16 div, css styling makes it fit in a grid
 for(let i = 0; i<16; i++){
-    const cell = document.createElement("div");
-    cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
-    container.appendChild(cell);
+    for(let j = 0; j<16; j++){
+        const cell = document.createElement("div");
+        cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        container.appendChild(cell);
+    }
 }
 // Find a way to make 16x16 grid of divs.
 
