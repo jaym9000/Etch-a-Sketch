0bd5eb1eb2dc1bba994da6e712a89d68333f488e
Jay-M <jean_marcc@hotmail.com>
2022-06-06 15:59:15 -0300

    16x16 div grid now done

--- sketch.js
+++ sketch.js
@@ -1,23 +1,10 @@
 const container = document.querySelector(".container");
 
-// for loop inside for loop creates a 16 x 16 div, css styling makes it fit in a grid
+// for loop inside for loop creates a 16 x 16 div, css styling makes each square 16x16 with a solid black line
 for(let i = 0; i<16; i++){
     for(let j = 0; j<16; j++){
         const cell = document.createElement("div");
-        cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+        cell.style.cssText = "border: 1px solid black; height: 16px; width: 16px";
         container.appendChild(cell);
     }
 }
-// Find a way to make 16x16 grid of divs.
-
-// using the container div in html, create 16x16 more divs using Javascript
-//after creating these divs, use CSS to make the divs look like a grid with the following options:
-
-/*
-float/clear
-inline-block
-flexbox
-CSS Grid
-
-(Probably go with flexbox or CSS Grid but learn all 4 to get familiar with them)
-*/
