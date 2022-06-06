c320698a578ae1eec66b877d91f5b14c9f7af0a0
Jay-M <jean_marcc@hotmail.com>
2022-06-06 18:57:43 -0300

    Git creation function made and called

--- sketch.js
+++ sketch.js
@@ -1,10 +1,20 @@
 const container = document.querySelector(".container");
 
 // for loop inside for loop creates a 16 x 16 div, css styling makes each square 16x16 with a solid black line
-for(let i = 0; i<16; i++){
-    for(let j = 0; j<16; j++){
-        const cell = document.createElement("div");
-        cell.style.cssText = "border: 1px solid black; height: 16px; width: 16px";
-        container.appendChild(cell);
+function divCreation(){
+    for(let i = 0; i<16; i++){
+        for(let j = 0; j<16; j++){
+            const cell = document.createElement("div");
+            cell.style.cssText = "border: 1px solid black; height: 16px; width: 16px";
+            container.appendChild(cell);
+        }
     }
 }
+
+// function hoverMouse(){
+//     divCreation.addEventListener("mouseenter", divCreation(){
+//         divCreation.target.style.color = "red";
+//         console.log("Works");
+//     });
+// }
+divCreation();
