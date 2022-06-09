cb642f2b1ca146b28f200c384d73ec4c96497c57
Jay-M <jean_marcc@hotmail.com>
2022-06-09 00:43:47 -0300

    Code was frustrating and confusing, restarted

--- sketch.js
+++ sketch.js
@@ -1,37 +1,7 @@
-const container = document.querySelector(".container");
-const resetBtn = document.querySelector(".btn");
+// Declaring const values
 
-// for loop inside for loop creates a 16 x 16 div, css styling makes each square 16x16 with a solid black line
-function divCreation() {
-  for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-      const cell = document.createElement("div");
-      cell.style.cssText = "border: 1px solid black; height: 16px; width: 16px";
-      container.appendChild(cell);
+// Declaring easier to use variables
 
-      hoverMouse(cell);
-    }
-  }
-}
+// Reuseable methods
 
-// highlight the mouseenter target
-// makes it so when u over over the boxes their background color changes.
-function hoverMouse(box) {
-  box.addEventListener(
-    "mouseenter",
-    function (e) {
-      box.style.backgroundColor = "red";
-    },
-    false
-  );
-}
-
-// Clears the box content when reset button is clicked
-function resetPage() {
-  container.textContent = "";
-  divCreation();
-}
-
-divCreation();
-
-resetBtn.addEventListener("click", resetPage);
+// Using DOM to get HTML Elements
