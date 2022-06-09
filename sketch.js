90252dbd376d48febdae4df7f8cdc68b200fd95f
Jay-M <jean_marcc@hotmail.com>
2022-06-08 22:43:23 -0300

    Reset button finished

--- sketch.js
+++ sketch.js
@@ -2,34 +2,36 @@ const container = document.querySelector(".container");
 const resetBtn = document.querySelector(".btn");
 
 // for loop inside for loop creates a 16 x 16 div, css styling makes each square 16x16 with a solid black line
-function divCreation(){
-    for(let i = 0; i<16; i++){
-        for(let j = 0; j<16; j++){
-            const cell = document.createElement("div");
-            cell.style.cssText = "border: 1px solid black; height: 16px; width: 16px";
-            container.appendChild(cell);
+function divCreation() {
+  for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
+      const cell = document.createElement("div");
+      cell.style.cssText = "border: 1px solid black; height: 16px; width: 16px";
+      container.appendChild(cell);
 
-            hoverMouse(cell);
-            
-        }
+      hoverMouse(cell);
     }
+  }
 }
 
-function hoverMouse(box){
-    // highlight the mouseenter target
-    box.addEventListener("mouseenter", function( e ) {   
-        box.style.backgroundColor = "red";
-    }, false);
+// highlight the mouseenter target
+// makes it so when u over over the boxes their background color changes.
+function hoverMouse(box) {
+  box.addEventListener(
+    "mouseenter",
+    function (e) {
+      box.style.backgroundColor = "red";
+    },
+    false
+  );
+}
+
+// Clears the box content when reset button is clicked
+function resetPage() {
+  container.textContent = "";
+  divCreation();
 }
 
 divCreation();
 
-function reloadPage(){
-    resetBtn.addEventListener("click", function(e){
-        // location.reload();
-        document.getElementById("container-id").reset();
-
-    });
-}
-
-reloadPage();
+resetBtn.addEventListener("click", resetPage);
