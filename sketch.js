2dbdbb5baa14539ce14783eeeeb0d4efd23ce9a9
Jay-M <jean_marcc@hotmail.com>
2022-06-09 22:52:35 -0300

    hover effect on grid to change color succeeded, next: grid size changements

--- sketch.js
+++ sketch.js
@@ -13,16 +13,26 @@ function resize(newSize) {
 // Using DOM to get HTML Elements
 const container = document.getElementById("container");
 
+// Event listeners
+
 // All specific functions
 function createDiv(size) {
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
     cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
     cell.classList.add("square");
+    cell.addEventListener("mouseenter", hoverChangeColor);
     container.appendChild(cell);
+
+    //change color of div on hover(event listener above activates this function on the grid)
+    function hoverChangeColor() {
+      cell.style.backgroundColor = "red";
+      console.log("Works");
+    }
   }
 }
 
+// When page loads, this displays by default
 window.onload = () => {
   createDiv(size);
 };
