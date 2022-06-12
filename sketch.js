c85fffa504ba40ad8df47aa39a5a1f4f0b99d047
Jay-M <jean_marcc@hotmail.com>
2022-06-11 21:44:55 -0300

    grid now stay same size even when number of divs is changed

--- sketch.js
+++ sketch.js
@@ -1,35 +1,44 @@
-// Declaring const values
+// SECTION: Declaring const values
 const DEFAULT_GRID_SIZE = 16;
 const DEFAULT_HOVER_COLOR = "#87CEEB";
 
-// Declaring easier to use variables
+// SECTION: Declaring easier to use variables
 let currentSize = DEFAULT_GRID_SIZE;
 
-// Reuseable methods
+// SECTION: Reuseable methods
 function resize(newSize) {
   size = newSize;
 }
 
-// Using DOM to get HTML Elements
+// SECTION: Using DOM to get HTML Elements
 const container = document.getElementById("container");
 const btn = document.getElementById("btn");
-const btnn = document.createElement("div");
+const slider = document.getElementById("sizeSlider");
+const gridSizeIndicator = document.getElementById("sizeVal");
 
-// Event listeners
+// SECTION: Event listeners
 btn.onclick = () => resetContainer();
+slider.onchange = (e) => changeGridSize(e);
+gridSizeIndicator.onchange = (e) => writeAboveSlider(e);
 
-// All specific functions
+// SECTION: All specific functions
 function createDiv(size) {
+  //   container.style.gridTemplateColumns = `repeat(${size}, 25px [col-start]`;
+  container.style.gridTemplateColumns = `repeat(${size}, 1fr`;
+  container.style.gridTemplateRows = `repeat(${size}, 1fr`;
   for (let i = 0; i < size * size; i++) {
     const cell = document.createElement("div");
-    cell.style.cssText = "border: 1px solid black; height: 25px; width: 25px";
+    cell.style.cssText = "border: 1px solid black;";
+    let widthxheight = parseInt(402 / size);
+    cell.style.width = `${widthxheight}px`;
+    cell.style.height = `${widthxheight}px`;
     cell.classList.add("square");
     cell.addEventListener("mouseenter", hoverChangeColor);
     container.appendChild(cell);
 
     //change color of div on hover(event listener above activates this function on the grid)
     function hoverChangeColor() {
-      cell.style.backgroundColor = "red";
+      cell.style.backgroundColor = "#00BFFF";
       console.log("Works");
     }
   }
@@ -46,9 +55,17 @@ function resetContainer() {
   createDiv(currentSize);
 }
 
+function changeGridSize(size) {
+  //   document.write(slider.value + "X" + slider.value);
+  console.log(slider.value);
+}
+
+function gridSizeInidcation(size) {
+  gridSizeIndicator.innerHTML = "";
+  gridSizeIndicator.innerHTML = `${slider.value} X ${slider.value}`;
+}
+
 // When page loads, this displays by default
 window.onload = () => {
   createDiv(currentSize);
 };
-
-// createDiv(size);
